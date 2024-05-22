4cb41feeea2b808bd01c983eb85f1f62a1e3a48d
sarvesh.kadam <kadamsarvesh1905@gmail.com>
2024-05-22 22:34:54 +0530

    config change

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,6 +20,7 @@ const io = new Server(http, {
     origin: "https://google-docs-ui-phi.vercel.app",
     methods: ["GET", "POST"],
   },
+  allowEIO3: true,
 });
 
 io.on("connection", (socket) => {
