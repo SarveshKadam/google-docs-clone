e1ef27ad982083ff4385d7087781f57596cb09ea
sarvesh.kadam <kadamsarvesh1905@gmail.com>
2024-05-23 01:22:15 +0530

    changing be url

--- frontend/src/components/TextEditor.tsx
+++ frontend/src/components/TextEditor.tsx
@@ -55,7 +55,7 @@ const Editor = () => {
   }, []);
 
   useEffect(() => {
-    const socketServer = io("https://google-docs-clone-phi.vercel.app", {
+    const socketServer = io("https://google-docs-clone-nq12.onrender.com", {
       transports: ["websocket", "polling"],
     });
     socketServer.on("connect_error", () => {
